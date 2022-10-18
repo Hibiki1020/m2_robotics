9fee1f04539d12f9f6efba2c63f106f07d8f4668
hibiki1020 <hibikijitaku@gmail.com>
2022-10-18 16:04:43 +0900

    Add listener

---
+++ robotics3/scripts/listener.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+#M1 mac上ではこのパスでないといけない
+import rospy
+from std_msgs.msg import String
+
+def callback(data):
+    rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
+
+rospy.init_node('listener', anonymous=True)
+sub = rospy.Subscriber('chatter', String, callback)
+rospy.spin()
