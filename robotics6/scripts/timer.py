b387a6a6837bd2d4571ac8fbf359baad5797dc11
hibiki1020 <hibikijitaku@gmail.com>
2022-11-15 16:36:50 +0900

    gitignore added

--- robotics6/scripts/timer.py
+++ robotics6/scripts/timer.py
@@ -10,7 +10,7 @@ import os
 import sys
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append('../')
-from msg import Call_timer
+from msg import *
 
 if __name__ == '__main__':
     rospy.init_node("timer", anonymous=True)
