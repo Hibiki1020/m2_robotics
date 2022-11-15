f0bcf6ffccee7113ee03c172bfb70172b4f6403b
hibiki1020 <hibikijitaku@gmail.com>
2022-11-15 16:15:41 +0900

    robotics6

---
+++ robotics6/scripts/listener.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+#M1mac
+import rospy
+from std_msgs.msg import String
+from robotics6.msg import Call_timer
+
+def callback(data):
+    rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
+
+def timer_callback(data):
+    rospy.loginfo(rospy.get_caller_id() + "I received %s", data.data)
+
+def callback_original(data):
+    rospy.loginfo(data)
+
+rospy.init_node('listener', anonymous=True)
+sub = rospy.Subscriber('chatter', String, callback)
+sub_timer = rospy.Subscriber('call_timer', String, timer_callback)
+sub_original = rospy.Subscriber('call_timer_original', Call_timer, callback_original)
+rospy.spin()
---
+++ robotics6/scripts/timer.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+#M1mac
+import rospy
+import math
+from std_msgs.msg import String
+from geometry_msgs.msg import Twist
+from turtlesim.msg import Pose
+from robotics6.msg import Call_timer
+
+
+if __name__ == '__main__':
+    rospy.init_node("timer", anonymous=True)
+    pub = rospy.Publisher("call_timer", String, queue_size=10)
+    pub_original = rospy.Publisher("call_timer_original", Call_timer, queue_size=10)
+
+    rate = rospy.Rate(10) # 10hz
+    start_time = rospy.get_time()
+    interval = 10.0
+
+    while not rospy.is_shutdown():
+        time_str = String()
+        original_time = Call_timer()
+        original_time.word = "Hello"
+        original_time.time = rospy.get_time()
+        pub_original.publish(original_time)
+
+        now = rospy.get_time()
+        if(now - start_time >= interval):
+            time_str.data = "time is %s [s]" % now
+            pub.publish(time_str)
+            start_time = now
+        rate.sleep()
